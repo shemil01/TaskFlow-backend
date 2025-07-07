365e869be3a8d7e530c85c716dc1d6a0da10c77a
shemil01 <shemilmk1033@gmail.com>
2025-07-07 18:24:38 +0530

    updated

--- controllers/Project.js
+++ controllers/Project.js
@@ -18,11 +18,10 @@ exports.createProject = async (req, res) => {
 
 //  get all projects
 exports.getAllProjects = async (req, res) => {
-
-  console.log(req.user.id)
-  const projects = await Project.find({ members: req.user.id }).populate(
-    "members"
-  ).populate('createdBy');
+  console.log(req.user.id);
+  const projects = await Project.find({ members: req.user.id })
+    .populate("members")
+    .populate("createdBy");
   if (!projects) {
     return res.status(404).json({
       success: false,
@@ -46,18 +45,24 @@ exports.getProjectById = async (req, res) => {
 
 // edit project
 exports.editProject = async (req, res) => {
-  const { projectId } = req.params;
+  const { projectId,userId } = req.params;
   const { name, description, members } = req.body;
+
+  const isUser = await User.findById(userId);
+
+  const updateFields = {};
+
+  if (name) updateFields.name = name;
+  if (description) updateFields.description = descript;
 
   const updatedProject = await Project.findByIdAndUpdate(
     projectId,
     {
-      ...(name && { name }),
-      ...(description && { description }),
-      ...(members && { members }),
+      $set: updateFields,
+      $addToSet: { members: userId },
     },
     { new: true, runValidators: true }
-  );
+  ).populate("members createdBy");
   if (!updatedProject) {
     return res.status(404).json({
       success: false,
@@ -67,6 +72,14 @@ exports.editProject = async (req, res) => {
   res.status(200).json({ success: true, project: updatedProject });
 };
 
+// // add members to project
+// exports.addMebers = async (req,res) => {
+//   const {userId}  = req.body
+//   const {projectId} = req.params
+
+//   const
+
+// }
 // deleate project
 exports.deleteProject = async (req, res) => {
   const { projectId } = req.params;
---
+++ controllers/user.js
@@ -0,0 +1,7 @@
+const User = require("../Models/User");
+
+
+// get all users
+exports.allUsers = async (req, res) => {
+    const users
+}
